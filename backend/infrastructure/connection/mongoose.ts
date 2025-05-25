eb029c1b2cc827b140c45597e655a68b3bba2816
مهدی عطایی <92110428+mahdiAtaee@users.noreply.github.com>
2025-05-25 11:29:45 +0330

    Update mongoose.ts

--- backend/infrastructure/connection/mongoose.ts
+++ backend/infrastructure/connection/mongoose.ts
@@ -1,10 +1,6 @@
 import * as mongoose from "mongoose";
 mongoose
-  .connect(process.env.MONGO_URL || "mongodb://localhost:27017/shop", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
+  .connect(process.env.MONGO_URL || "mongodb://localhost:27017/shop")
   .then(() => {
     console.log("connection is open...");
   })
