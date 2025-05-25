7602c26962e15b40d3761b3858b19119aac8bdf0
مهدی عطایی <92110428+mahdiAtaee@users.noreply.github.com>
2025-05-25 12:02:27 +0330

    Update index.ts

--- backend/src/boot/index.ts
+++ backend/src/boot/index.ts
@@ -4,7 +4,13 @@ import * as fileUpload from "express-fileupload";
 import { Application, static as static_root } from "express";
 
 export default function boot(app: Application) {
-  app.use(cors());
+  app.use(cors(
+    {
+      origin: ["https://shop-seven-mocha.vercel.app"],
+      methods: ["POST","GET","PUT","PATCH"],
+      credentials:true
+    }
+  ));
   app.use(bodyParser.json());
   app.use(fileUpload());
   app.use(static_root("public"));
