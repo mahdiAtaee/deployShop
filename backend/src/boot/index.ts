2c4929af187963443e482e4ad96bcb493343df36
mahdi <mahdiataee1689@gmail.com>
2025-05-26 17:57:48 +0330

    add git ignore

--- backend/src/boot/index.ts
+++ backend/src/boot/index.ts
@@ -2,6 +2,7 @@ import * as cors from "cors";
 import * as bodyParser from "body-parser";
 import * as fileUpload from "express-fileupload";
 import { Application, static as static_root } from "express";
+import { join } from "path";
 
 export default function boot(app: Application) {
   app.use(cors(
